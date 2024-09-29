912626261e69bec95d02f6e2cb164dcd3dd8a625
vSparkyy <75097362+vSparkyy@users.noreply.github.com>
2024-09-29 23:04:38 +0100

    initial push

---
+++ main.py
@@ -0,0 +1,31 @@
+import pygame
+import scenes
+import sys
+
+
+WIDTH, HEIGHT = (1400, 900)
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("OCRtunes")
+pygame.init()
+pygame.key.set_repeat(250, 50)
+
+clock = pygame.time.Clock()
+current_scene = scenes.MainMenu()
+
+while True:
+    clock.tick(60)
+    events = pygame.event.get()
+    for event in events:
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+    current_scene.process_input(events)
+    current_scene.update()
+    current_scene.render(WIN)
+    pygame.display.update()
+
+    next_scene = current_scene.next_scene
+    if next_scene is not current_scene:
+        current_scene.next_scene = current_scene
+        current_scene = next_scene
